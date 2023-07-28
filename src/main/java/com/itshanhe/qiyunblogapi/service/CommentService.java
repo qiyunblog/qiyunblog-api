1332140fa82ee77ab55bb3b7b13a79eb2adb4d25
shanhe <shanhel@qq.com>
2023-07-28 21:47:36 +0800

    文章，评论部分内容

---
+++ src/main/java/com/itshanhe/qiyunblogapi/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.itshanhe.qiyunblogapi.service;
+
+/**
+ * 评论业务 未完成
+ */
+public interface CommentService {
+    
+    int addComment();
+}
--- src/main/java/com/itshanhe/qiyunblogapi/service/ContentService.java
+++ src/main/java/com/itshanhe/qiyunblogapi/service/ContentService.java
@@ -4,7 +4,18 @@ package com.itshanhe.qiyunblogapi.service;
  * 文章相关接口
  */
 public interface ContentService {
+    /**
+     * 添加文章
+     * @param htmlBody
+     * @param id
+     * @return
+     */
     int addArticle(String htmlBody,String id);
     
+    /**
+     * 删除文章
+     * @param id
+     * @return
+     */
     int deleteArticle(String id);
 }
